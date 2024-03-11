e820579b0e6ed4b6a462fe0cfb9f21a9129cf49c
DianaPavlova <diana50888@gmail.com>
2024-03-11 07:53:54 +0200

    Добавляет общий подвал с навигацией.

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,5 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
+// import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {initVideo} from './modules/video/init-video';
 import {initAccordions} from './modules/accordion/init-accordion';
@@ -34,13 +34,12 @@ window.addEventListener('DOMContentLoaded', () => {
   const form = new Form();
   form.init();
   window.form = form;
-  window.form.validateForm(form);
 
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
+   // initModals();
     // const form = new Form();
     // window.form = form;
     // form.init();
